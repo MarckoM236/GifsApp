21034c5647a6aeef9e8a9c9e0f173fd3843cf742
unknown <marcko236@gmail.com>
2023-04-18 12:49:55 -0500

    Se guarda y se carga  el historial en el localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -18,7 +18,24 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor( private http:HttpClient){}
+  constructor( private http:HttpClient){
+    this.loadLocalStorage();
+  }
+
+  //almacenar en el local storage
+  private saveLocalstorage():void{
+    localStorage.setItem('history',JSON.stringify(this._historial));
+  }
+
+  //cargar lo selementos del localStorage
+  private loadLocalStorage():void{
+    if(!localStorage.getItem('history')){return;}
+
+    this._historial=JSON.parse(localStorage.getItem('history')!);
+
+    if(this._historial.length===0){return;}
+    this.buscarGifs(this._historial[0]);
+  }
 
   //ordenar el historial de busqueda
   organizeHistory(query:string):void{
@@ -27,10 +44,12 @@ export class GifsService {
     if(!this._historial.includes(query)){ //includes: true si el elemento Existe.
       this._historial.unshift(query);
       this._historial=this._historial.splice(0,10);//almacenar solo 10 elementos
+      this.saveLocalstorage();
     } 
+
   }
 
-
+ //Realizar petricion http segun parametro de busqueda.
   buscarGifs(query:string):void{
 
     this.organizeHistory(query);
